73d58b60fcf4bbc72018027ee154fcb19af3456f
XOPEXAIT <ilya0410@outlook.com>
2024-12-02 18:44:03 +0100

    add Project Structure

---
+++ src/main/java/com/illia/plantsPlacementPlugin/Main.java
@@ -0,0 +1,22 @@
+package com.illia.plantsPlacementPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        PlantsPlacementCommand pluginCommandPlantsPlacement = new PlantsPlacementCommand();
+        Objects.requireNonNull(this.getCommand("plantsplacement")).setExecutor(pluginCommandPlantsPlacement);
+        getServer().getPluginManager().registerEvents(new PlantsPlacementListener(pluginCommandPlantsPlacement), this);
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/com/illia/plantsPlacementPlugin/PlantsPlacementListener.java
@@ -0,0 +1,97 @@
+package com.illia.plantsPlacementPlugin;
+
+import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+import java.util.EnumSet;
+import java.util.Set;
+
+public class PlantsPlacementListener implements Listener {
+    private final PlantsPlacementCommand pluginCommandPlantsPlacement;
+
+    public PlantsPlacementListener(PlantsPlacementCommand pluginCommandPlantsPlacement) {
+        this.pluginCommandPlantsPlacement = pluginCommandPlantsPlacement;
+    }
+
+    private static final Set<Material> PLANT_MATERIALS = EnumSet.of(
+            Material.SHORT_GRASS,
+            Material.TALL_GRASS,
+            Material.FERN,
+            Material.LARGE_FERN,
+            Material.OAK_SAPLING,
+            Material.SPRUCE_SAPLING,
+            Material.BIRCH_SAPLING,
+            Material.JUNGLE_SAPLING,
+            Material.ACACIA_SAPLING,
+            Material.DARK_OAK_SAPLING,
+            Material.MANGROVE_PROPAGULE,
+            Material.CHERRY_SAPLING,
+            Material.AZALEA,
+            Material.FLOWERING_AZALEA,
+            Material.BROWN_MUSHROOM,
+            Material.RED_MUSHROOM,
+            Material.CRIMSON_FUNGUS,
+            Material.WARPED_FUNGUS,
+            Material.DEAD_BUSH,
+            Material.DANDELION,
+            Material.POPPY,
+            Material.BLUE_ORCHID,
+            Material.ALLIUM,
+            Material.AZURE_BLUET,
+            Material.ORANGE_TULIP,
+            Material.PINK_TULIP,
+            Material.RED_TULIP,
+            Material.WHITE_TULIP,
+            Material.OXEYE_DAISY,
+            Material.CORNFLOWER,
+            Material.LILY_OF_THE_VALLEY,
+            Material.TORCHFLOWER,
+            Material.WITHER_ROSE,
+            Material.PINK_PETALS,
+            Material.SPORE_BLOSSOM,
+            Material.BAMBOO,
+            Material.SUGAR_CANE,
+            Material.CACTUS,
+            Material.CRIMSON_ROOTS,
+            Material.WARPED_ROOTS,
+            Material.NETHER_SPROUTS,
+            Material.WEEPING_VINES,
+            Material.TWISTING_VINES,
+            Material.SUNFLOWER,
+            Material.LILAC,
+            Material.ROSE_BUSH,
+            Material.PEONY,
+            Material.PITCHER_PLANT,
+            Material.BIG_DRIPLEAF,
+            Material.SMALL_DRIPLEAF,
+            Material.CHORUS_PLANT,
+            Material.CHORUS_FLOWER,
+            Material.GLOW_LICHEN,
+            Material.HANGING_ROOTS
+    );
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        if (!pluginCommandPlantsPlacement.isAllowPlantsPlacement()) return;
+        if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            Block clickedBlock = event.getClickedBlock();
+            Player player = event.getPlayer();
+
+            if (PLANT_MATERIALS.contains(player.getInventory().getItemInMainHand().getType())) {
+                Block blockUnder = clickedBlock.getRelative(BlockFace.UP);
+                blockUnder.setType(player.getInventory().getItemInMainHand().getType());
+                if (player.getGameMode() == GameMode.SURVIVAL) {
+                    player.getInventory().getItemInMainHand().setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
+                    event.setCancelled(true);
+                }
+            }
+        }
+    }
+}
