54b640c685b07ef8aac48ca4ae81a88455c34124
XOPEXAIT <ilya0410@outlook.com>
2025-01-10 14:16:35 +0100

    add Project Structure

--- src/main/java/com/illia/plantsPlacementPlugin/PlantsPlacementCommand.java
+++ src/main/java/com/illia/plantsPlacementPlugin/PlantsPlacementCommand.java
@@ -1,12 +1,33 @@
 package com.illia.plantsPlacementPlugin;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.permissions.PermissionAttachmentInfo;
 import org.jetbrains.annotations.NotNull;
 
 public class PlantsPlacementCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
+        Player player = (Player) sender;
+        if (!hasWealthPermission(player)) {
+            sender.sendMessage(ChatColor.RED + "Вам потрібна підписка " + ChatColor.AQUA + "Wealth " + ChatColor.RED + "для цього!");
+            return false;
+        }
+
+        allowPlantsPlacement = !allowPlantsPlacement;
+
+        if (allowPlantsPlacement) {
+            player.sendMessage("Розміщення рослин на будь які блоки ввімкнено");
+        } else {
+            player.sendMessage("Розміщення рослин на будь які блоки вимкнено.");
+        }
+
+        return true;
+    }
 
     private boolean allowPlantsPlacement = false;
 
@@ -14,21 +35,14 @@ public class PlantsPlacementCommand implements CommandExecutor {
         return allowPlantsPlacement;
     }
 
-    @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
-        if (sender instanceof Player) {
-            Player player = (Player) sender;
-
-            allowPlantsPlacement = !allowPlantsPlacement;
-
-            if (allowPlantsPlacement) {
-                player.sendMessage("Plants placement enabled.");
-            } else {
-                player.sendMessage("Plants placement disabled.");
+    public static boolean hasWealthPermission(Player player) {
+        for (PermissionAttachmentInfo permissionInfo : player.getEffectivePermissions()) {
+            String permission = permissionInfo.getPermission();
+            if (permission.startsWith("Wealth")) {
+                return true;
             }
-
-            return true;
         }
         return false;
     }
+
 }
